3e9c81e9a2cdb5ab47a3a0cf23afe9f2c9cc5f92
Gabriel-Paulino-Alves <128816401+Gabriel-Paulino-Alves@users.noreply.github.com>
2023-06-30 13:00:38 -0300

    Update CartaoDAO.java

--- Trabalho/src/model/dao/CartaoDAO.java
+++ Trabalho/src/model/dao/CartaoDAO.java
@@ -6,19 +6,21 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 import model.entity.Cartao;
+import model.entity.Pessoa;
 
 public class CartaoDAO {
 
 
 	public void inserirCartao(Cartao cartao) {
 			ConexaoBd con = new ConexaoBd();
-			String sql = "INSERT INTO cartao (dataValidade, numeroCartao, limiteTotal, limiteDisponivel) VALUES (?,?,?,?)";
+			String sql = "INSERT INTO cartao (dataValidade, numeroCartao, limiteTotal, limiteDisponivel, idPessoa) VALUES (?,?,?,?,?)";
 			try {
 				PreparedStatement pst = con.getConexao().prepareStatement(sql);
 				pst.setString(1, cartao.getDataV());
 	            pst.setString(2, cartao.getNumeroC());
 	            pst.setDouble(3, cartao.getLimiteC());
 	            pst.setDouble(4, cartao.getLimiteDisp());
+	            pst.setInt(5, cartao.getIdPessoa());
 	            pst.execute();
 	            System.out.println("Cartao principal inserido");
 			} catch (SQLException e){
@@ -34,12 +36,21 @@ public class CartaoDAO {
             pst.setInt(1, idCartao);
             ResultSet rs =  pst.executeQuery();
             if (rs.next()){
+            	//String nome = rs.getString("nome");
+            	//String cpf = rs.getString("cpf");
                 String dataV = rs.getString("dataValidade");
                 String numero = rs.getString("numeroCartao");
                 String limiteTotal = rs.getString("limiteTotal");
-                String limiteDisponivel = rs.getString("limiteDisponivel");
-                c = new Cartao(dataV,numero,limiteTotal,limiteDisponivel);
-                c.setId(rs.getInt("id Cartao"));
+                String id = rs.getString("idPessoa");
+                int idP = Integer.parseInt(id);
+                PessoaDAO dao = new PessoaDAO();
+                Pessoa p = dao.consultar(idP);
+                
+                //String limiteDisponivel = rs.getString("limiteDisponivel");
+                double l = Double.parseDouble(limiteTotal);
+                //double ld = Double.parseDouble(limiteDisponivel);
+                c = new Cartao(p.getNome(),p.getCPF(),dataV,numero,l,idP);
+                c.setId(rs.getInt("idCartao"));
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -57,11 +68,15 @@ public class CartaoDAO {
             while (rs.next()){
                 Cartao cartao = new Cartao();
                 int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String cpf = rs.getString("cpf");
                 String dataV = rs.getString("dataValidade");
                 String numero = rs.getString("numeroCartao");
                 Double limiteTotal = rs.getDouble("limiteTotal");
                 Double limiteDisponivel = rs.getDouble("limiteDisponivel");
                 cartao.setIdCartao(id);
+                cartao.setNome(nome);
+                cartao.setCPF(cpf);
                 cartao.setDataV(dataV);
                 cartao.setNumeroC(numero);
                 cartao.setLimiteC(limiteTotal);
@@ -75,7 +90,7 @@ public class CartaoDAO {
     }
 	
 	public boolean excluirCartao(int chave){
-        String sql = "DELETE FROM cartao WHERE id = ?";
+        String sql = "DELETE FROM cartao WHERE idCartao = ?";
         try{
         	ConexaoBd conexao = new ConexaoBd();
             PreparedStatement pst = conexao.getConexao().prepareStatement(sql);
@@ -90,7 +105,7 @@ public class CartaoDAO {
     }
 	public boolean atualizar(Cartao cartao){
         try {
-            String sql = "UPDATE cartao SET dataValidade = ?, numeroCartao = ?, limiteTotal = ?, limiteDisponivel = ? WHERE id = ?";
+            String sql = "UPDATE cartao SET dataValidade = ?, numeroCartao = ?, limiteTotal = ?, limiteDisponivel = ? WHERE idCartao = ?";
             ConexaoBd conexao = new ConexaoBd();
             PreparedStatement pst = conexao.getConexao().prepareStatement(sql);
             pst.setString(1, cartao.getDataV());
